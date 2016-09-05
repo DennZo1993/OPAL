7e57f2da8482afaf2a572d8025770e20337f1403
Denis Zobnin <d.zobnin.work@gmail.com>
2016-09-05 23:34:36 +0300

    Untabify test/MatrixTest.cpp. NFC.

--- test/MatrixTest.cpp
+++ test/MatrixTest.cpp
@@ -8,8 +8,8 @@ TEST(MatrixTests, ReadTest) {
   ASSERT_EQ(3, m.getHeight());
   ASSERT_EQ(4, m.getWidth());
   for (int i = 0; i < m.getHeight(); ++i) {
-  	for (int j = 0; j < m.getWidth(); ++j) {
-  		ASSERT_DOUBLE_EQ(1.0, m[i][j]);
-  	}
+    for (int j = 0; j < m.getWidth(); ++j) {
+      ASSERT_DOUBLE_EQ(1.0, m[i][j]);
+    }
   }
 }
