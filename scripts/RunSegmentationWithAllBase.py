3f8e44e5d6bfd27f51939aecdd75b5eeda2ed2f7
Denis <dennzo1993@gmail.com>
2016-11-26 20:29:50 +0500

    Show results statistics in run script + use Python 3.

--- scripts/RunSegmentationWithAllBase.py
+++ scripts/RunSegmentationWithAllBase.py
@@ -1,5 +1,7 @@
 import os
+import re
 import subprocess
+import json
 from shutil import rmtree
 
 from config import make_opal_config
@@ -10,6 +12,34 @@ IMAGES_BASE = '../test/test_data/Images'
 SEGMENTATION_BASE = '../test/test_data/Segmentations'
 RESULTS_ROOT_DIR = 'results'
 
+global_times = []
+global_scores = {}
+
+
+def get_results_from_log(target_idx, scores, times):
+  dice_scores = []
+  running_time = ''
+
+  log_file = os.path.join(RESULTS_ROOT_DIR, str(target_idx), 'log.txt')
+  with open(log_file, 'r') as log:
+    for l in log:
+      line = l.rstrip()
+      if re.search('[0-9]+\t[0-9]+(\.[0-9]+)?', line):
+        dice_scores.append(line)
+      elif 'OPAL running time:' in line:
+        _, running_time = line.strip().split(': ')
+  
+  for line in dice_scores:
+    label, score = line.strip().split('\t')
+    label = int(label)
+    score = float(score)
+    if label in scores:
+      scores[label].append(score)
+    else:
+      scores[label] = [score]
+
+  times.append(float(running_time))
+
 
 def run_segmentation(config, images, segmentations, target_idx):
   '''
@@ -18,6 +48,7 @@ def run_segmentation(config, images, segmentations, target_idx):
        <base_image_1> <base_seg_1>
        ...
        <base_image_n> <base_seg_n>
+       <result_dir>
   '''
   args = [OPAL_EXE, config,
           os.path.join(IMAGES_BASE, images[target_idx]),
@@ -39,11 +70,40 @@ def run_segmentation(config, images, segmentations, target_idx):
   with open(current_log_file, 'w') as log:
     subprocess.call(args, stdout=log)
 
+  get_results_from_log(target_idx, global_scores, global_times)
+
+
+def results_stats():
+  import statistics
+  mean_running_time = statistics.mean(global_times)
+  mean_scores = {}
+  median_scores = {}
+  min_scores = {}
+  max_scores = {}
+  for label, score_list in global_scores.items():
+    if label < 2:
+      continue;
+    min_scores[label] = min(score_list)
+    max_scores[label] = max(score_list)
+    mean_scores[label] = statistics.mean(score_list)
+    median_scores[label] = statistics.median(score_list)
+
+  print('{:<10}{:<10}{:<10}{:<10}{:<10}'.format('Label', 'Min', 'Max', 'Mean', 'Median'))
+  for label in global_scores:
+    if label < 2:
+      continue
+    print('{:<10}{:<10.5}{:<10.5}{:<10.5}{:<10.5}'.format(label,
+                                                          min_scores[label],
+                                                          max_scores[label],
+                                                          mean_scores[label],
+                                                          median_scores[label]))
+
+  print(mean_running_time)
+
 
 def main():
   if not os.path.exists(OPAL_EXE):
     raise ValueError('Path to OPAL executable "{s}" is incorrect!'.format(s=OPAL_EXE))
-
 
   if not os.path.exists(IMAGES_BASE):
     raise ValueError('Path to images database "{s}" is incorrect!'.format(s=IMAGES_BASE))
@@ -58,10 +118,10 @@ def main():
 
   opal_settings = {
     'initWindowRadius': '10',
-    'patchRadius': 12,
+    'patchRadius': 6,
     'intermediateSaving': 'false',
     'intermediateSavingPath': '',
-    'maxIterations': '30',
+    'maxIterations': '50',
   }
 
   rmtree(RESULTS_ROOT_DIR, ignore_errors=True)
@@ -69,8 +129,19 @@ def main():
   opal_config = os.path.join(RESULTS_ROOT_DIR, 'config.txt')
   make_opal_config(opal_config, **opal_settings)
 
+  print('Running OPAL...')
   for i in range(len(images)):
     run_segmentation(opal_config, images, segmentations, i)
+    print('Image {i} done'.format(i=i))
+
+  results = {}
+  results['times'] = global_times
+  results['scores'] = global_scores
+  with open('results.json', 'w') as f:
+    json.dump(results, f, sort_keys=True, indent=4, separators=(',', ':'))
+
+  results_stats()
+
 
 if '__main__' == __name__:
   main()
--- scripts/config.py
+++ scripts/config.py
@@ -1,4 +1,4 @@
 def make_opal_config(config_file, **kwargs):
   with open(config_file, 'w') as f:
-    for name, value in kwargs.iteritems():
+    for name, value in kwargs.items():
       f.write('{n}={v}\n'.format(n=name, v=value))
