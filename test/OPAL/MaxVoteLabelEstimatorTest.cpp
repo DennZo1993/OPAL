fbf43edf311bc86f4c33d3f5c0f2a8ad794c69fa
Denis Zobnin <d.zobnin.work@gmail.com>
2017-04-02 01:27:07 +0300

    Add test case for MaxVoteLabelEstimator.

--- test/OPAL/MaxVoteLabelEstimatorTest.cpp
+++ test/OPAL/MaxVoteLabelEstimatorTest.cpp
@@ -17,3 +17,38 @@ TEST(LabelEstimators, Test1) {
 
   ASSERT_EQ(3, estimator.EstimateLabel(candidates));
 }
+
+TEST(LabelEstimators, Test2) {
+  using EstimatorType = MaxVoteLabelEstimator<int>;
+  EstimatorType::CandidateContainer candidates = {
+    { 41, 1.0 },
+    { 43, 1.0 },
+    { 43, 1.0 },
+    { 43, 1.0 },
+    {  0, 1.0 },
+    { 41, 1.0 },
+    { 43, 1.0 },
+    { 43, 1.0 },
+    {  0, 1.0 },
+    {  0, 1.0 },
+    {  0, 1.0 },
+    {  0, 1.0 },
+    {  0, 1.0 },
+    {  0, 1.0 },
+    {  0, 1.0 },
+    {  0, 1.0 },
+    {  0, 1.0 },
+    {  0, 1.0 },
+    {  0, 1.0 },
+    {  0, 1.0 },
+    { 49, 1.0 },
+    {  0, 1.0 },
+    {  0, 1.0 },
+    {  0, 1.0 },
+    {  0, 1.0 }
+  };
+
+  EstimatorType estimator;
+
+  ASSERT_EQ(0, estimator.EstimateLabel(candidates));
+}
