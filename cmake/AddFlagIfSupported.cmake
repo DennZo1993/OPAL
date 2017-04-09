2f008bbbcf55152429c6551dda64db8fd777399f
Denis Zobnin <d.zobnin.work@gmail.com>
2017-04-09 20:45:21 +0300

    CMake: fix add_flag_if_supported with MSVC

--- cmake/AddFlagIfSupported.cmake
+++ cmake/AddFlagIfSupported.cmake
@@ -7,7 +7,7 @@ include(CheckCXXCompilerFlag)
 
 function(add_flag_if_supported FLAG OUTPUT_PARAM)
   check_cxx_compiler_flag(${FLAG} COMPILER_SUPPORTS_FLAG)
-  if(COMPILER_SUPPORTS_FLAG)
+  if(COMPILER_SUPPORTS_FLAG AND NOT MSVC)
     set(${OUTPUT_PARAM} ${${OUTPUT_PARAM}} ${FLAG} PARENT_SCOPE)
   else()
     message("-- C++ compiler doesn't support ${FLAG}")
